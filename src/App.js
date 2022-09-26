a3e74fcb01eaa5ecaf939451a13486c7cf2c262f
qalbehabib <rana.habib313@gmail.com>
2022-09-26 19:24:05 +0500

    upadted packages

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React, { Component } from 'react';
-import './App.css';
-import MainPage from "./components/mainpage"
-import io from 'socket.io-client';
-import {BrowserRouter,Route} from "react-router-dom";
-import { Provider } from 'react-redux'
-import store from './redux'
-let l=window.location.origin;
- let loc=(l==="http://localhost:3000")?"http://localhost:800":window.location.origin;
+import React, { Component } from "react";
+import "./App.css";
+import MainPage from "./components/mainpage";
+import io from "socket.io-client";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux";
+let l = window.location.origin;
+let loc =
+  l === "http://localhost:3000"
+    ? "http://localhost:800"
+    : window.location.origin;
 const socket = io(loc);
 
 class App extends Component {
-  componentWillMount(){
-    this.setState({number:Math.random()});
-   
+  componentWillMount() {
+    this.setState({ number: Math.random() });
   }
   // componentWillUnmount(){
   //   this.done()
   // }
-done=()=>{
- 
-fetch(`/del/${this.state.number}`,{method:"DELETE"})
-.then(res=>res.text())
-.then(res=>alert(res))
-.catch(err=>console.log(err));
-}
+  done = () => {
+    fetch(`/del/${this.state.number}`, { method: "DELETE" })
+      .then((res) => res.text())
+      .then((res) => alert(res))
+      .catch((err) => console.log(err));
+  };
   render() {
     return (
       <Provider store={store}>
-      <div className="App">
-      <BrowserRouter>
-      <MainPage number={this.state.number} socket={socket}></MainPage>
-      </BrowserRouter>
-      </div>
+        <div className="App">
+          <BrowserRouter>
+            <MainPage number={this.state.number} socket={socket}></MainPage>
+          </BrowserRouter>
+        </div>
       </Provider>
     );
   }
